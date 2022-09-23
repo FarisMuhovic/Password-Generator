0c1c39b0c6adbc6afcc91755da22b537f5eae55e
FarisMuhovic <faris.fare123456@gmail.com>
2022-09-23 23:57:55 +0200

    v3

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // *** Password Length Slider *** //
 const rangeInput = document.getElementById("rangeinput");
 const numberInput = document.getElementById("inputnumbers");
-
+let root = document.documentElement;
 rangeInput.oninput = function () {
   numberInput.value = rangeInput.value;
+  // *** Input Range Color *** //
+  let cssVars = getComputedStyle(document.documentElement).getPropertyValue(
+    "--sliderpercentage"
+  );
+  let percentage = Number(rangeInput.value) * 2.5 - 25;
+  root.style.setProperty("--sliderpercentage", String(percentage + "%"));
 };
-
 numberInput.oninput = function () {
   rangeInput.value = numberInput.value;
+  // *** Input Range Color *** //
+  let cssVars = getComputedStyle(document.documentElement).getPropertyValue(
+    "--sliderpercentage"
+  );
+  let percentage = Number(rangeInput.value) * 2.5 - 25;
+  root.style.setProperty("--sliderpercentage", String(percentage + "%"));
 };

48458121597afb0ed88d551809641451cf82dfe2
FarisMuhovic <faris.fare123456@gmail.com>
2022-09-24 21:44:43 +0200

    func

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ numberInput.oninput = function () {
   let percentage = Number(rangeInput.value) * 2.5 - 25;
   root.style.setProperty("--sliderpercentage", String(percentage + "%"));
 };
+
+// *** Password types for checkboxes *** //
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_><+=-";
+//new variable
+const generateBtn = document.getElementById("generatebtn");
+generateBtn.addEventListener("click", function () {
+  let lengthOfPassword = rangeInput.value;
+  let password = "";
+  let mix = "";
+  let upperCheckbox = document.getElementById("upper").checked;
+  if (upperCheckbox === true) {
+    mix += upperCase;
+  }
+  let lowerCheckbox = document.getElementById("lower").checked;
+  if (lowerCheckbox === true) {
+    mix += lowerCase;
+  }
+  let numberCheckbox = document.getElementById("number").checked;
+  if (numberCheckbox === true) {
+    mix += numbers;
+  }
+  let symbolCheckbox = document.getElementById("symbol").checked;
+  if (symbolCheckbox === true) {
+    mix += symbols;
+  }
+  let i = 0;
+  while (i < lengthOfPassword) {
+    i++;
+    let randomNum = Math.floor(Math.random() * mix.length);
+    password += mix[randomNum];
+  }
+  document.getElementById("passwordOutput").value = password;
+});
